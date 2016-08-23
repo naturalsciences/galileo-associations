1bef58c7b60229010f5c5f9b39e5d4ed065001c9
Paul-André Duchesne <Paul-Andre.Duchesne@naturalsciences.be>
2016-08-23 18:38:40 +0200

    Start of gulp and bower integration + other stuffs that these tools can bring

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+var del = require('del');
+var Q = require('q');
+
+gulp.task('default', function() {
+    console.log('GULP THIS!');
+});
