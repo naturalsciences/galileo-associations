ba157af05dd6b9ad9a31225225d94005b992052e
Paul-André Duchesne <PolloFox@gmail.com>
2016-09-08 00:56:37 +0200

    Install of jsRouting bundle from FriendsOfSymfony

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new \Lunetics\LocaleBundle\LuneticsLocaleBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
             new AppBundle\AppBundle(),
         ];
 
