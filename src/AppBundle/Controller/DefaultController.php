ae0d31240d450d1c95b5fb09ce8671e652d1a99a
Paul-André Duchesne <Paul-Andre.Duchesne@naturalsciences.be>
2016-09-09 18:18:46 +0200

    Added a search spinner indication + basis of xhttp search call
    refs #1980

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,37 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @var Request Complete request object
+     * @return Response A Response instance of the form of a twig template filled in
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
+    }
+
+    /**
+     * @var Request Complete request object
+     * @return JsonResponse A Response instance of the form of a json
+     */
+    public function fastSearchAction(Request $request) {
+        $results = array();
+        $response = new JsonResponse();
+        if (
+            $request->isXmlHttpRequest() &&
+            $request->get('fast_search_type') !== null
+        ){
+        }
+        $response->setData($results);
+        return $response;
     }
 }
