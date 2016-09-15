23c7eaea5fb7dae48c368bdbc76d6a788c27ab3d
Paul-André Duchesne <Paul-Andre.Duchesne@naturalsciences.be>
2016-09-15 16:26:22 +0200

    Applied receipe from Symfony Cookbook for redirecting pages with trailing /
    fix #2001

--- src/AppBundle/Controller/FastSearchController.php
+++ src/AppBundle/Controller/FastSearchController.php
@@ -10,7 +10,6 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class FastSearchController extends Controller
---
+++ src/AppBundle/Controller/RedirectingController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: duchesne
+ * Date: 15/09/16
+ * Time: 16:23
+ */
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class RedirectingController extends Controller
+{
+    public function removeTrailingSlashAction(Request $request)
+    {
+        $pathInfo = $request->getPathInfo();
+        $requestUri = $request->getRequestUri();
+
+        $url = str_replace($pathInfo, rtrim($pathInfo, ' /'), $requestUri);
+
+        return $this->redirect($url, 301);
+    }
+}
