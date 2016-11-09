f37ff1ac1c90b290585f22d995feb6d2bc6a9ff6
Paul-André Duchesne <Paul-Andre.Duchesne@naturalsciences.be>
2016-11-09 13:34:23 +0100

    Collapse and expand functional for lettered content list for person, projects and teams
    refs #1986,#1987,#2011

--- app/Resources/assets/js/base.js
+++ app/Resources/assets/js/base.js
@@ -11,5 +11,18 @@ $(document).ready(
                 $(this).parent('.alert-dismissible').fadeOut();
             }
         );
+
+        $('.rbins-cards-actions-open').on(
+            'click',
+            function (){
+                $(this).toggle().next('.rbins-cards-actions-close').toggle();
+            }
+        );
+        $('.rbins-cards-actions-close').on(
+            'click',
+            function (){
+                $(this).toggle().prev('.rbins-cards-actions-open').toggle();
+            }
+        );
     }
 );
