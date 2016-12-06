df3732045bc6d8246441a5d7b3a02939c698dc5d
Paul-André Duchesne <Paul-Andre.Duchesne@naturalsciences.be>
2016-12-06 12:33:48 +0100

    Stylin of login form done
    fix #2029

--- src/AppBundle/Form/Type/SecurityFormType.php
+++ src/AppBundle/Form/Type/SecurityFormType.php
@@ -4,13 +4,29 @@ namespace AppBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class SecurityFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('_username')
-            ->add('_password', PasswordType::class);
+        $builder
+        ->add(
+            '_username',
+            TextType::class,
+            array(
+                'label' => 'login.user_label',
+                'translation_domain' => 'security'
+            )
+        )
+        ->add(
+            '_password',
+            PasswordType::class,
+            array(
+                'label' => 'login.pwd_label',
+                'translation_domain' => 'security'
+            )
+        );
     }
 }
